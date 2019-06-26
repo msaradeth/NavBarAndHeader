6765e7750c83290866455106108380d626370a7d
Mike Saradeth <mikesaradeth@yahoo.com>
2019-06-26 06:16:34 -0700

    UICollectionViewCell - use contentView.subview to calc cell height

--- NavBarAndHeader/Detail/DetailCell.swift
+++ NavBarAndHeader/Detail/DetailCell.swift
@@ -40,39 +40,38 @@ class DetailCell: UICollectionViewCell {
     }
     
     private func setupViews() {
-        addSubview(titleLabel)
+        contentView.translatesAutoresizingMaskIntoConstraints = false
+        contentView.fillSuperview()
+        
+        contentView.addSubview(titleLabel)
         titleLabel.topAnchor.constraint(equalTo: topAnchor).isActive = true
         titleLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: padding).isActive = true
         titleLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -padding).isActive = true
         
-        addSubview(detailLabel)
+        contentView.addSubview(detailLabel)
         detailLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 8).isActive = true
         detailLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: padding).isActive = true
         detailLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -padding).isActive = true
-        detailLabel.setContentHuggingPriority(.defaultLow, for: .vertical) 
+        detailLabel.setContentHuggingPriority(.defaultLow, for: .vertical)
     }
     
     override func preferredLayoutAttributesFitting(_ layoutAttributes: UICollectionViewLayoutAttributes) -> UICollectionViewLayoutAttributes {
         super.preferredLayoutAttributesFitting(layoutAttributes)
+
+        //calc cell height - add up all subviews height of contentView
+        let cellHeight = contentView.subviews.reduce(0, {$0 + $1.bounds.height})
         
-        //calc cell height - add up all subview height except index 0 which is contentView
-        var cellHeight: CGFloat = 0
-        for index in 1..<subviews.count {
-            cellHeight += subviews[index].bounds.height
-        }
-   
         //set cell height and width
         layoutAttributes.frame.size.height = max(cellHeight, minCellHeight)
         layoutAttributes.frame.size.width  = (superview?.bounds.width ?? 0) - 40
         return layoutAttributes
     }
-
+    
     override func prepareForReuse() {
         super.prepareForReuse()
         titleLabel.text = nil
         detailLabel.text = nil
     }
-
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
